cf2aa063a3334ec2649f10875b72c88a826d5bae
jaimeibanezrivera <jaimeibanezrivera@gmail.com>
2025-12-26 00:46:51 +0100

    Fixed Alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -23,11 +23,11 @@ return {
 
     -- Set menu
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
-      dashboard.button("r", "  Recently used files", ":Telescope oldfiles<CR>"),
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+      dashboard.button("r", "  Recently used files", ":Telescope oldfiles<CR>"),
       dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
-      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+      dashboard.button("q", "󰩈  Quit Neovim", ":qa<CR>"),
     }
 
 
