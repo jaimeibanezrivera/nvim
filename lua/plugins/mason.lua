47b42375d17a511a67877a9a6d94bc54e56abdd7
jaimeibanezrivera <jaimeibanezrivera@gmail.com>
2026-01-19 22:06:38 +0100

    Adding mason.lua in linux personal pc, this one works

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,29 @@
+-- ~/.config/nvim/lua/plugins/mason.lua
+return {
+  "williamboman/mason.nvim",
+  build = ":MasonUpdate",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    })
+    
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "pyright",
+        "tsserver",
+        -- Add other servers you want
+      },
+      automatic_installation = true,
+    })
+  end
+}
