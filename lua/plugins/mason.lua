33d432eb7467431db56ba1a7e1257418ae96a144
jaimeibanezrivera <jaimeibanezrivera@gmail.com>
2026-01-21 22:37:06 +0100

    fixed mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,27 +1,10 @@
 return {
-  "williamboman/mason.nvim",
-  build = ":MasonUpdate",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    require("mason").setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-        }
-      }
-    })
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "pyright",
-        "tsserver",
-        -- Add other servers you want
-      },
-      automatic_installation = true,
-    })
-  end
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+        require("mason").setup()
+	end,
 }
