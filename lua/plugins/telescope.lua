1ea4f4b8ab067f74b623eb1a2084204f2b810bcd
jaimeibanezrivera <jaimeibanezrivera@gmail.com>
2026-01-21 21:43:55 +0100

    added new keybindings to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,6 +14,8 @@ return{
       vim.keymap.set('n', '<leader>bf', builtin.find_files, {})  -- Browse for file
       vim.keymap.set('n', '<leader>bt', builtin.live_grep, {})  -- Browse for Text
       vim.keymap.set('n', '<leader>be', require('telescope.builtin').diagnostics, { desc = "Browse Errors" }) -- Browse Errors
+      vim.keymap.set('n', '<leader>br', builtin.oldfiles, { desc = "Browse Recent files" }) -- Browse Recent
+      vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = "Browse Buffers" }) -- Browse Buffers
 
     end
   },
