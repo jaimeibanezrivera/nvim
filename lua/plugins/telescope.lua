f2deeffb13f2c9f56ed40eace51c6a381fa10731
Ibanez Rivera Jaime <jaime.ibanez_rivera@nokia.com>
2026-03-27 19:20:57 +0100

    fixing issues with lsp
    fixing problem with lsp :)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,10 @@
-return{
+return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      -- Ensure Telescope is properly loaded before setting up
-      local status, telescope = pcall(require, "telescope")
-      if not status then
-        vim.notify("Telescope not found!", vim.log.levels.ERROR)
-        return
-      end
+      local telescope = require("telescope")
       
-      -- Configure Telescope layout
       telescope.setup({
         defaults = {
           layout_strategy = 'vertical',
@@ -27,32 +21,29 @@ return{
       
       local builtin = require("telescope.builtin")
       
-      -- Function to find only hidden files
       local function find_hidden_files()
         builtin.find_files({
           hidden = true,
-          file_ignore_patterns = { "^[^.]" }, -- Ignore files that don't start with a dot
+          file_ignore_patterns = { "^[^.]" },
           prompt_title = "Find Hidden Files"
         })
       end
       
-      vim.keymap.set('n', '<leader>bf', builtin.find_files, {})  -- Browse for file
-      vim.keymap.set('n', '<leader>bh', find_hidden_files, { desc = "Browse Hidden files" })  -- Browse Hidden only
-      vim.keymap.set('n', '<leader>bt', builtin.live_grep, {})  -- Browse for Text
-      vim.keymap.set('n', '<leader>be', builtin.diagnostics, { desc = "Browse Errors" }) -- Browse Errors
-      vim.keymap.set('n', '<leader>br', builtin.oldfiles, { desc = "Browse Recent files" }) -- Browse Recent
-      vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = "Browse Buffers" }) -- Browse Buffers
+      vim.keymap.set('n', '<leader>bf', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>bh', find_hidden_files, { desc = "Browse Hidden files" })
+      vim.keymap.set('n', '<leader>bt', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>be', builtin.diagnostics, { desc = "Browse Errors" })
+      vim.keymap.set('n', '<leader>br', builtin.oldfiles, { desc = "Browse Recent files" })
+      vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = "Browse Buffers" })
     end
   },
   {
     'nvim-telescope/telescope-ui-select.nvim',
     config = function()
-      require("telescope").setup ({
+      require("telescope").setup({
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-              -- even more opts
-            }
+            require("telescope.themes").get_dropdown {}
           }
         }
       })
