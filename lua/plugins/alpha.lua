87d63216ae26ab801faf30d997b51aaa86f4c2f9
Ibanez Rivera Jaime <jaime.ibanez_rivera@nokia.com>
2026-04-16 17:51:15 +0200

    updated alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,43 +1,45 @@
 return {
-  "goolord/alpha-nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
-    -- Set header
-    dashboard.section.header.val = {
-      "                                                     ",
-      "                                                     ",
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
-      "                                                     ",
-      "                                                     ",
-    }
-    -- Set menu
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "  Find file", ":FzfLua files<CR>"),
-      dashboard.button("r", "  Recently used files", ":FzfLua oldfiles<CR>"),
-      dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
-      dashboard.button("q", "󰩈  Quit Neovim", ":qa<CR>"),
-    }
-    -- Set footer
-    dashboard.section.footer.val = {
-        "                             ",
-        "                             ",
-        "                             ",
-        "                             ",
-        "Hi Jaumas, welcome to Neovim!"
-        }
-    -- Send config to alpha
-    alpha.setup(dashboard.opts)
-    -- Disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  end,
+	"goolord/alpha-nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+		-- Set header
+		dashboard.section.header.val = {
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                       ]],
+	[[                                                                     ]],
+	[[       ████ ██████           █████      ██                     ]],
+	[[      ███████████             █████                             ]],
+	[[      █████████ ███████████████████ ███   ███████████   ]],
+	[[     █████████  ███    █████████████ █████ ██████████████   ]],
+	[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+	[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+	[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+	[[                                                                       ]],
+}
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "  Find file", ":FzfLua files<CR>"),
+			dashboard.button("r", "  Recently used files", ":FzfLua oldfiles<CR>"),
+			dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
+			dashboard.button("q", "󰩈  Quit Neovim", ":qa<CR>"),
+		}
+		-- Set footer
+		dashboard.section.footer.val = {
+			"                             ",
+			"                             ",
+			"                             ",
+			"                             ",
+			"Hi Jaumas, welcome to Neovim!",
+		}
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
 }
