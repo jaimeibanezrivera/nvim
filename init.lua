6b1771c7e676fb9e752524584809386731756d23
jaimeibanezrivera <jaimeibanezrivera@gmail.com>
2026-05-30 22:16:05 +0200

    init: get into the error

--- init.lua
+++ init.lua
@@ -60,6 +60,10 @@ vim.keymap.set("n", "]e", function() vim.diagnostic.goto_next({ severity = vim.d
 vim.keymap.set("n", "[e", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end, { desc = "Go to previous error" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+vim.keymap.set("n", "<leader>e", function()
+    local _, win = vim.diagnostic.open_float()
+    if win then vim.api.nvim_set_current_win(win) end
+end, { desc = "Focus diagnostic float" })
 
 -- Splits
 vim.keymap.set("n", "<leader>s", "<cmd>vsplit<CR>", { desc = "Vertical split" })
